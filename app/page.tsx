83642182f074e0fb458478ce7fe2d70fb628670c
William Hou <hou.w.77@gmail.com>
2025-01-29 23:29:09 -0500

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import snoopers from '../public/kermit/snoopers.png';
 
 export default function Home() {
-  const [ playing, setPlaying ] = useState(false);
+  const [playing, setPlaying] = useState(false);
   const audioRef: RefObject<HTMLAudioElement | null> = useRef(null);
   function playAudio() {
     if (playing) {
@@ -18,11 +18,15 @@ export default function Home() {
   }
 
   return (
-    <div className={`page grid items-center justify-items-center min-h-screen max-h-screen ${playing ? '' : 'hover:cursor-pointer'}`} onClick={playAudio}>
-      <h1>Rebecca Sparks, will you be my <span>baby</span> and my Valentine?</h1>
-      <div className="contain-size flowers"></div>
-      <Image className="contain-size" src={snoopers} alt="snoopy <3"/>
-      <audio ref={audioRef} src="/kermit/take3.mp3" preload="auto"/>
+    <div className={`page min-h-screen max-h-screen ${playing ? '' : 'hover:cursor-pointer'}`} onClick={playAudio}>
+      <div className="center grid items-center justify-items-center">
+        <h1>Rebecca Sparks, will you be my <span>baby</span> and my Valentine?</h1>
+        <Image className="contain-size" src={snoopers} alt="snoopy <3" />
+      </div>
+      <div className="decor">
+        <div className="contain-size flowers"></div>
+      </div>
+      <audio ref={audioRef} src="/kermit/take3.mp3" preload="auto" />
     </div>
   );
 }
