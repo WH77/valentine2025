dcd6126401a363481b85bbb8b630e5184d3dad5e
William Hou <hou.w.77@gmail.com>
2025-01-29 22:27:18 -0500

    tweak audio

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 'use client'
 
-import { RefObject, useRef } from "react";
+import { RefObject, useRef, useState } from "react";
 
 export default function Home() {
+  const [ playing, setPlaying ] = useState(false);
   const audioRef: RefObject<HTMLAudioElement | null> = useRef(null);
   function playAudio() {
+    if (playing) {
+      return;
+    }
+
     audioRef.current?.fastSeek(0);
     audioRef.current?.play();
+    setPlaying(true);
   }
 
   return (
-    <div className="grid items-center justify-items-center min-h-screen">
-      <h1 className="hover:cursor-pointer" onClick={playAudio}>Rebecca Sparks, will you be my <span>baby</span> and my Valentine?</h1>
+    <div className="grid items-center justify-items-center min-h-screen" onClick={playAudio}>
+      <h1>Rebecca Sparks, will you be my <span>baby</span> and my Valentine?</h1>
       <div className="flowers"></div>
       <audio ref={audioRef} src="/kermit/take3.mp3" preload="auto"/>
     </div>
